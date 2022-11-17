d243cf5f72457efad4948a3ee6dd8f8c11091762
100Ubuntu <potterd123@gmail.com>
2022-11-17 13:57:16 -0800

    new data

--- newGen.py
+++ newGen.py
@@ -1,10 +1,11 @@
 import yahoo_fin.stock_info as si 
 
+toWrite = ""
 
 startDate = "2021-01-01"
 endDate = "2021-12-31"
 
-writeToFile = open("data2021.txt", "a")
+writeToFile = open("data2021_2.txt", "a")
 
 def getTickerStartDateData(t):
     data = si.get_data(t, start_date = startDate, end_date = endDate, index_as_date = True, interval="1d")
@@ -14,15 +15,22 @@ def getTickerStartDateData(t):
     endDateClose = data.get("close")[-1]
     percentGain = (endDateClose - startDateOpen) / startDateOpen
     variance = 0
+    global toWrite
     for day in range(0, len(data)):
         dayPercentVar = (highs[day] - lows[day]) / highs[day]
         variance += dayPercentVar
-    writeToFile.write(str(t) + " " + str(percentGain) + " " + str(variance) + "\n")
+    toWrite += (str(t) + " " + str(percentGain) + " " + str(variance) + "\n")
+    
 
 
 f = open("tickers.txt", 'r')
 running = False
 while True:
+    if len(toWrite) > 10000:
+        print("writing")
+        copy = toWrite
+        writeToFile.write(copy)
+        toWrite = ""
     ticker = f.readline().strip()
     if running:
         if not ticker:
@@ -33,9 +41,9 @@ while True:
             continue
 
 
-    if ticker == "ANGO":
+    if ticker == "DWAS":
         running = True
-    
+
     
 f.close()
 writeToFile.close()
